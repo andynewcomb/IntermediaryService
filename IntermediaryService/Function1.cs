3d239fb37847a2d2a4971ba16875494b4325cdbb
Andrew Newcomb <Andrew.Newcomb@cayuse.com>
2019-12-01 15:59:24 -0800

    Made change to see if continuous integration is working

--- IntermediaryService/Function1.cs
+++ IntermediaryService/Function1.cs
@@ -27,7 +27,7 @@ namespace IntermediaryService
 
             return name != null
                 ? (ActionResult)new OkObjectResult($"Hello, {name}")
-                : new BadRequestObjectResult("Please pass a name on the query string or in the request body");
+                : new BadRequestObjectResult("Testing continuous integration");
         }
     }
 }
